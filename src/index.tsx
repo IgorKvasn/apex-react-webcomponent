c7b926a9ef623464fec6adbb70585d1646298092
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-29 20:16:46 +0100

    aa

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import "./index.css";
 import App from "./App";
 import ReactWebComponent from "react-web-component";
 
+if (process.env.NODE_ENV === "development") {
+  // @ts-ignore
+  window.nnCssFiles = [
+    "https://rawcdn.githack.com/IgorKvasn/apex-react-webcomponent/dd53c4f75063ff88e48181d1a9b588f2d35050a3/build/static/css/main.54f89c75.chunk.css",
+  ];
+}
+
 if (!customElements.get("my-component")) {
   ReactWebComponent.create(
     <React.StrictMode>
