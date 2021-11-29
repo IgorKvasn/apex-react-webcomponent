a30ca228ca86e93674a7d52e60579221cbc4020f
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-29 19:48:15 +0100

    x

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,8 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
 import ReactWebComponent from "react-web-component";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-/* const App = withRouter(({ history }) => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/topics" component={Topics} />
-  </div>
-));
- */
 if (!customElements.get("my-component")) {
   ReactWebComponent.create(
     <React.StrictMode>
@@ -39,8 +11,3 @@ if (!customElements.get("my-component")) {
     "my-component"
   );
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
