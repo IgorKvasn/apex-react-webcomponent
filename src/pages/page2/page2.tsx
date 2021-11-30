fb137eae5e5000688ffa37247f3bc6f33fb401be
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-30 12:01:37 +0100

    ajax + flatpick

--- src/pages/page2/page2.tsx
+++ src/pages/page2/page2.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.css";
 
 function Page2() {
-  return <div>Ja som page 2</div>;
+  const inputElem = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputElem.current) {
+      flatpickr(inputElem.current);
+    }
+  }, [inputElem]);
+
+  function performRequest() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <>
+      <h1>Ja som page 2</h1>
+      <button onClick={performRequest}>Do request</button>
+      <input ref={inputElem} />
+    </>
+  );
 }
 
 export default Page2;
